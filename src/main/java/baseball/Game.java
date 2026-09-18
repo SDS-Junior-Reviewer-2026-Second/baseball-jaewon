22113fc9dad2937179833a8f3d304ab0910be70d
wonnowone <harmonace10@gmail.com>
2026-09-18 11:28:15 +0900

    invaildinput

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,4 +2,13 @@ package baseball;
 
 public class Game {
 
+    public void guess(String guessNumber) {
+        if (guessNumber == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (guessNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
